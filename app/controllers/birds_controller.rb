9b7e5fcc5c2e5167db08b0db9df21e0d8047767e
Bevin Duncan <bevin.duncan@gmail.com>
2022-04-05 18:05:28 -0700

    commit tree a338b805f69d780d256e46ad237671b5c141c1ce

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -6,6 +6,21 @@ class BirdsController < ApplicationController
     render json: birds
   end
 
+  #PATCH /birds/:id
+def update
+  bird = Bird.find_by(id: params[:id])
+  if bird
+    bird.update(bird_params)
+    render json: bird
+  else
+    render json: { error: "Bird not found" }, status: :not_found
+  end
+end
+
+def bird_params
+  params.permit(:name, :species, :likes)
+end
+
   # POST /birds
   def create
     bird = Bird.create(bird_params)
